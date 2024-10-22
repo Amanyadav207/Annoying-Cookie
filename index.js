104a1e77ce0c788f768cecb9a8162b4e0d9a4244
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-10-22 13:05:52 +0530

    close maded working

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
 
+const closebtn = document.getElementById("modal-close-btn");
+
+closebtn.addEventListener("click",()=>{
+        modal.style.display='none';
+  
+})
